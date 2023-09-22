6a26aa2963421cca8e0d15321ee301628609e08a
emanuelongo <emanuel1001cano@gmail.com>
2023-09-21 19:04:12 -0500

    backend

---
+++ Nerdle/Modelo/backend.py
@@ -0,0 +1,41 @@
+class Nerdle:
+    def __init__(self, secuencia):
+        pass
+
+    def generar_elemento(self):
+        pass
+
+    def mostrar_secuencia(self):
+        pass
+
+    def mostrar_intentos_restantes(self):
+        pass
+
+    def reducir_intentos(self):
+        pass
+
+    def retroalimentar(self):
+        pass
+
+    def volver_a_jugar(self):
+        pass
+
+    def terminar_juego(self):
+        pass
+
+
+class Jugador:
+    def configurar_intentos(self, cantidad):
+        pass
+
+    def adivinar_secuencia(self, secuencia):
+        pass
+
+    def secuencia_no_adivinada(self):
+        pass
+
+    def iniciar_nuevo_juego(self, opcion):
+        pass
+
+    def
+
